a07ed2122f7843f465170e6f2db26fdcba87a971
Artem <gameepictv@gmail.com>
2023-10-15 00:47:07 +0600

    #2 Создал приветственное сообщение по имени пользователя на команду /start в телеграм боте.

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message: telebot.types.Message):
+    bot.reply_to(message, f"Howdy, how are you doing {message.from_user.first_name}?")
