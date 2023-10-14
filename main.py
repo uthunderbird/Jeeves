83a90588d52b0fbbef6d419fb0249aee0e8b1e24
Artem <gameepictv@gmail.com>
2023-10-15 00:49:10 +0600

    #3 Написал хэндлер для отлавливания сообщения от пользователя

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message: telebot.types.Message):
     bot.reply_to(message, f"Howdy, how are you doing {message.from_user.first_name}?")
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message: telebot.types.Message):
+    user_message = message.text
