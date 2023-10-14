9ea3e09bffd7af342cf011e2072ae6e0977cfcc4
Artem <gameepictv@gmail.com>
2023-10-15 00:53:14 +0600

    #5 Подключил openAI через библиотеку langChain

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import telebot
 import os
 from dotenv import load_dotenv
 
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo",
+                 openai_api_key=OPENAI_API_KEY)
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
@@ -19,3 +27,6 @@ def send_welcome(message: telebot.types.Message):
 def handle_text(message: telebot.types.Message):
     user_message = message.text
     bot.reply_to(message, user_message)
+
+
+bot.infinity_polling()
